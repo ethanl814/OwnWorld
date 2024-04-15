5d9f4ed1a5af5fd493627410a7a65ace7b270a56
Ethan Liu <ethanlmails@gmail.com>
2024-04-15 01:49:34 -0700

    3

--- proj3/src/core/Main.java
+++ proj3/src/core/Main.java
@@ -4,11 +4,14 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-//        Scanner scan = new Scanner(System.in);
-//        System.out.println("Enter valid string with format \"N#######S\"");
-//        String info = scan.nextLine();
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter valid string with format \"N#######S\"");
+        String info = scan.nextLine();
+        World world = getWorldFromInput(info);
     }
-    public void getWorldFromInput(String info ) {
-
+    public static World getWorldFromInput(String info) {
+        String numberStr = info.substring(1, info.length() - 1);  // Remove the first and last character
+        int number = Integer.parseInt(numberStr);
+        return new World(number);
     }
 }
