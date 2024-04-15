ed424f6f137c7dee57c608c7174aa3d64be0d2dc
Ethan Liu <ethanlmails@gmail.com>
2024-04-15 01:41:16 -0700

    3

--- proj3/src/core/Main.java
+++ proj3/src/core/Main.java
@@ -1,10 +1,14 @@
 package core;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-
-        // build your own world!
-
+//        Scanner scan = new Scanner(System.in);
+//        System.out.println("Enter valid string with format \"N#######S\"");
+//        String info = scan.nextLine();
+    }
+    public void getWorldFromInput(String info ) {
 
     }
 }
