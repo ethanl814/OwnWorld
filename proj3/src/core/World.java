f12f4a04d3a85c3304ece582cf3bbdf7e4839a2a
Ethan Liu <ethanlmails@gmail.com>
2024-04-15 01:31:35 -0700

    3

--- proj3/src/core/World.java
+++ proj3/src/core/World.java
@@ -1,7 +1,43 @@
 package core;
 
+import tileengine.TERenderer;
+import tileengine.TETile;
+import tileengine.Tileset;
+
+import java.util.Random;
+
 public class World {
+    static final int DEFAULT_WIDTH = 50;
+    static final int DEFAULT_HEIGHT = 50;
+    TERenderer ter;
+    TETile[][] board;
+    Random random;
+    int width;
+    int height;
+    private boolean isGameOver;
+    public World(long seed) {
+        width = DEFAULT_WIDTH;
+        height = DEFAULT_HEIGHT;
+        ter = new TERenderer();
+        ter.initialize(width, height);
+        random = new Random(seed);
+        TETile[][] nothingWorld = new TETile[width][height];
+        fillWithNothing(nothingWorld);
+        board = nothingWorld;
+    }
+    public void fillWithNothing(TETile[][] tiles) {
+        int height = tiles[0].length;
+        int width = tiles.length;
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                tiles[x][y] = Tileset.NOTHING;
+            }
+        }
+    }
+    public void putHallway() {
 
-    // build your own world!
+    }
+    public void putRoom() {
 
+    }
 }
